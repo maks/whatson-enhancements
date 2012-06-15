ff44334292a4241eb586c9df119e786ac6dde827
Maksim Lin <maks@manichord.com>
2012-06-15 15:52:28 +1000

    added JS for calendar wdiget and setup.

---
+++ asset551892-calendar.js
@@ -0,0 +1,29 @@
+<div id="calendar"></div>
+
+<script src="%globals_asset_href:110430%"></script>
+<script src="%globals_asset_href:551895%"></script>
+
+<script>
+    var weekRange = function(dateText) {
+      var d = Date.parseExact(dateText, "yyyy-M-d");
+      return d.last().monday().toString("yyyy-M-d")+","+d.next().sunday().next().sunday().toString("yyyy-M-d");
+    }
+
+    $('#calendar').datepicker({
+        inline: true,
+        firstDay: 1,
+        showOtherMonths: false,
+        minDate: new Date(),
+        hideIfNoPrevNext: true,
+        dateFormat: "yy-mm-dd",
+        dayNamesMin: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+        onSelect: function(dateText, inst) {
+            window.location.search = "period="+weekRange(dateText);
+        }
+    });
+    var startDateParam = window.location.search.match(/(\d\d\d\d-\d+-\d+),/);
+    if (startDateParam && startDateParam[1]) {
+       $('#calendar').datepicker("setDate", startDateParam[1]);
+    }
+
+</script>
